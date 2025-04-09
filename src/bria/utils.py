2dfbbfb204fdd43354608c1d250d1d5e6862fde4
er1cw00 <wadahana@qq.com>
2025-04-09 15:51:34 +0800

    feat: add bria v1.4

---
+++ src/bria/utils.py
@@ -0,0 +1,8 @@
+import torch.nn.functional as F
+
+## upsample tensor 'src' to have the same spatial size with tensor 'tar'
+def _upsample_like(src,tar):
+
+    src = F.interpolate(src,size=tar.shape[2:],mode='bilinear')
+
+    return src
